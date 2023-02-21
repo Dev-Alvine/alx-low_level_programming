4b731448577b9e7439c3ad2e39974c90624f0a8a
Alvine Mwashi <alvinemwa99@gmail.com>
2023-02-21 13:26:29 -0800

    2-print_alphabet_x10.c

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,15 +1,21 @@
-#include<stdo.h>
 
-  int main(){
-   putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
+
+#include <stdio.h>
+
+int main() {
+    printf("_putchar\n");
     return 0;
-  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
