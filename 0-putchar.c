0f8b9436c4724b14c57671cd39c835e3b563e8e1
Alvine Mwashi <alvinemwa99@gmail.com>
2023-02-21 00:19:32 -0800

    Added my project

---
+++ 0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdo.h>
+
+  int main(){
+   putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    return 0;
+  }
+
