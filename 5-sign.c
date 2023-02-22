0177fe5e132b87a069e9e1f1f5c7460c7515c07e
Alvine Mwashi <alvinemwa99@gmail.com>
2023-02-22 06:10:27 -0800

    Task 5. Sign

---
+++ 5-sign.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int print_sign(int n) {
+    if (n > 0) {
+        printf("+");
+        return 1;
+    } else if (n < 0) {
+        printf("-");
+        return -1;
+    } else {
+        printf("0");
+        return 0;
+    }
+}
+
